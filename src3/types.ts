4cdd161a889cc53bd4e104c20279cacd559890ce
hugo <hugo.denisse@gmail.com>
2019-07-22 00:01:55 +0200

    add move

--- src3/types.ts
+++ src3/types.ts
@@ -1,30 +1,31 @@
-type PieceState = { type: string; color: number; x: number; y: number };
+export type PieceState = { type: string; color: Color; x: number; y: number };
 
-type BoardState = {
-  whoseTurn: number;
+export type BoardState = {
+  whoseTurn: Color;
   pieces: PieceState[];
 };
 
-type Square = {
+export type Square = {
   x: number;
   y: number;
-  color: number;
+  color: Color;
   piece: PieceState;
 };
+
+export type Color = 0 | 1;
 
 const WIDTH = 8;
 const HEIGHT = 8;
 
-class Board {
+export class Board {
   private state: BoardState;
   private squares: Square[];
 
-  constructor(state: BoardState) {
+  constructor(state: BoardState = null) {
     const len = WIDTH * HEIGHT;
     let x: number;
-    let y: number;
-    let color: number;
-
+    let y: number = 0;
+    let color: Color;
     this.squares = [];
     for (let i: number = 0; i < len; i++) {
       x = i % WIDTH;
@@ -76,5 +77,42 @@ class Board {
 
   getSquare(x: number, y: number): Square {
     return this.squares[x + y * WIDTH];
+  }
+
+  move(x: number, y: number, toX: number, toY: number): void {
+    const piece: PieceState = this.getSquare(x, y).piece;
+    if (!piece) {
+      throw new Error(`No piece found in ${x},${y}`);
+    }
+    if (piece.color !== this.state.whoseTurn) {
+      throw new Error(
+        `The piece found in ${x},${y} does not belong to the current player`
+      );
+    }
+
+    // do all the verification stuff
+
+    let killed: PieceState = null;
+
+    // dispatch move and kill events
+
+    this.setState({
+      whoseTurn: ((this.state.whoseTurn + 1) % 2) as Color,
+      pieces: this.state.pieces.map((p: PieceState) => {
+        if (p === piece) {
+          return {
+            type: p.type,
+            color: p.color,
+            x: toX,
+            y: toY,
+          };
+        } else if (p === killed) {
+          // on met le state de la piece tuée à null
+          return null;
+        } else {
+          return p;
+        }
+      }),
+    });
   }
 }
