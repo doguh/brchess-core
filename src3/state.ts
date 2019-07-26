f29bbbd4f176bc151faee5c66a35604111b59044
hugo <hugo.denisse@gmail.com>
2019-07-26 20:15:32 +0200

    add getRandomBoardState

--- src3/state.ts
+++ src3/state.ts
@@ -1,4 +1,4 @@
-import { BoardState, Color } from './types';
+import { BoardState, Color, PieceState } from './types';
 import { White, Black } from './constantes';
 
 export default function getDefaultBoardState(): BoardState {
@@ -207,4 +207,66 @@ export default function getDefaultBoardState(): BoardState {
       },
     ],
   };
+}
+
+export function getRandomBoardState(): BoardState {
+  const state: BoardState = getDefaultBoardState();
+  // on place les pieces aléatoirement
+  state.pieces.forEach(piece => {
+    const newPos = getRandomPos(piece);
+    moveAndSwap(piece, newPos.x, newPos.y, state.pieces);
+  });
+
+  // on vérifie que les 2 fous d'un même joueur
+  // ne sont pas sur une case de même couleur
+  let whiteBishop: number = -1;
+  let blackBishop: number = -1;
+  state.pieces.forEach(piece => {
+    if (piece.type === 'b') {
+      const color: number = piece.x % 2 === piece.y % 2 ? 0 : 1;
+      const matesColor: number =
+        piece.color === White ? whiteBishop : blackBishop;
+      if (color === matesColor) {
+        // les 2 fous sont sur une case de même couleur, on décale le 2e d'une case
+        moveAndSwap(piece, (piece.x + 1) % 8, piece.y, state.pieces);
+      }
+      if (piece.color === White) {
+        whiteBishop = color;
+      } else {
+        blackBishop = color;
+      }
+    }
+  });
+  return state;
+}
+
+function moveAndSwap(
+  piece: PieceState,
+  x: number,
+  y: number,
+  pieces: PieceState[]
+): void {
+  const swap: PieceState = getPieceAt(pieces, x, y);
+  if (swap) {
+    swap.x = piece.x;
+    swap.y = piece.y;
+  }
+  piece.x = x;
+  piece.y = y;
+}
+
+function getPieceAt(pieces: any, x: number, y: number): PieceState {
+  // pieces est de type any parce que TypeScript ne semble pas être au courant que Array.find existe depuis 150 ans
+  return pieces.find((piece: PieceState) => piece.x === x && piece.y === y);
+}
+
+function getRandomPos(piece: PieceState): { x: number; y: number } {
+  const out = {
+    x: Math.floor(Math.random() * 8),
+    y: Math.floor(Math.random() * 3),
+  };
+  if (piece.color === Black) {
+    out.y = 7 - out.y;
+  }
+  return out;
 }
